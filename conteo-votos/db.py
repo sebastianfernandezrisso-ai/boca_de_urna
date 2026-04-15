545c93b86c104337112ea4852935f3412fb9c2fd
sebastianfernandezrisso-ai <sebastianfernandezrisso@gmail.com>
2026-04-15 11:12:26 -0300

    Update db.py

--- conteo-votos/db.py
+++ conteo-votos/db.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = st.secrets["DATABASE_URL"]
+DATABASE_URL ="postgresql://postgres.vjhpuimpxssekyhoqwqu:Matanza2941@aws-0-us-west-2.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(
     DATABASE_URL,
@@ -13,18 +13,18 @@ def create_table():
     with engine.begin() as conn:
         conn.execute(text("""
             CREATE TABLE IF NOT EXISTS mesas (
-                id SERIAL PRIMARY KEY,
-                mesa TEXT UNIQUE,
-                sede TEXT,
-                localidad TEXT,
-                movimiento INTEGER,
-                lista2 INTEGER,
-                lista3 INTEGER,
-                blanco INTEGER,
-                impugnados INTEGER,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            );
+    id SERIAL PRIMARY KEY,
+    mesa TEXT UNIQUE,
+    sede TEXT,
+    localidad TEXT,
+    movimiento INTEGER,
+    lista2 INTEGER,
+    
+    blanco INTEGER,
+    impugnados INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    verificado BOOLEAN DEFAULT FALSE
+);
         """))
-
-
+    
 
